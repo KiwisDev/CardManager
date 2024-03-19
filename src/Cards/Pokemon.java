33df1fa5b4bdd992ad280ba289f2e5dbd8949b06
Aiman Manchout <magicaiman@MacBook-Air-de-Aiman.local>
2024-03-19 11:31:32 +0100

    Pokemon classe

--- src/Cards/Pokemon.java
+++ src/Cards/Pokemon.java
@@ -1,4 +1,22 @@
 package Cards;
 
-public class Pokemon {
+public class Pokemon extends Card{
+    private String element;
+    private int force;
+
+    public String getElement() {
+        return element;
+    }
+
+    public void setElement(String element) {
+        this.element = element;
+    }
+
+    public int getForce() {
+        return force;
+    }
+
+    public void setForce(int force) {
+        this.force = force;
+    }
 }
