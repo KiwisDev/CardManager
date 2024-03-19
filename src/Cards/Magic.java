fdccffcdec591aa380c317cc8a2bd00b34a970bc
Aiman Manchout <magicaiman@MacBook-Air-de-Aiman.local>
2024-03-19 11:33:13 +0100

    Magic classe

--- src/Cards/Magic.java
+++ src/Cards/Magic.java
@@ -1,4 +1,32 @@
 package Cards;
 
-public class Magic {
+public class Magic extends Card{
+    private int force;
+    private int rang;
+    private String couleur;
+
+
+    public int getForce() {
+        return force;
+    }
+
+    public void setForce(int force) {
+        this.force = force;
+    }
+
+    public int getRang() {
+        return rang;
+    }
+
+    public void setRang(int rang) {
+        this.rang = rang;
+    }
+
+    public String getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(String couleur) {
+        this.couleur = couleur;
+    }
 }
