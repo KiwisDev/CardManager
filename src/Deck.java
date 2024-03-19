97ad9f6be7d57b9210fdb1957664d7e705a800da
Aiman Manchout <magicaiman@MacBook-Air-de-Aiman.local>
2024-03-19 11:55:16 +0100

    getPokemon deck function

--- src/Deck.java
+++ src/Deck.java
@@ -1,4 +1,5 @@
 import Cards.Card;
+import Cards.CardType;
 
 import java.util.ArrayList;
 
@@ -8,4 +9,14 @@ public class Deck {
     public void getAll(){
         cardsList.forEach((n)->System.out.println("Le nom est :"+n.getName()));
     }
+
+    public void getPokemon(){
+        for(int i=0;i< cardsList.size();i++){
+            if(cardsList.get(i).getType()== CardType.POKEMON){
+                System.out.println("Le nom est : "+cardsList.get(i).getName());
+            }
+        }
+    }
+
+
 }
